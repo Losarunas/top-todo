2d042c9fe262b92a9181e8d3dab63697a0b468e1
SarunasL <losarunas@gmail.com>
2022-03-12 19:16:42 +0200

    show todos

--- src/index.js
+++ src/index.js
@@ -15,11 +15,21 @@ app.addProject(defaultProject);
 app.addProject(workProject);
 app.addProject(studyProject);
 
-const todo1 = new Todo("Dance", "2015 - 01 - 15", "2015 - 01 - 25", 2);
-const todo2 = new Todo("Swim in ocean", format(new Date(), "yyyy-MMM-dd"), "2018 - 01 - 25", 3);
+const todo1 = new Todo("Dance", "2015 - 01 - 15", format(new Date(), "yyyy-MM-dd"), 2);
+const todo2 = new Todo("Swim in ocean", format(new Date(), "yyyy-MM-dd"), "2018 - 01 - 25", 3);
+const todo3 = new Todo("Read", format(new Date(), "yyyy-MM-dd"), "2018 - 01 - 25", 3);
+const todo4 = new Todo("Sleep", format(new Date(), "yyyy-MM-dd"), "2018 - 01 - 25", 3);
+const todo5 = new Todo("Sing", format(new Date(), "yyyy-MM-dd"), "2018 - 01 - 25", 3);
+const todo6 = new Todo("Go for a walk", format(new Date(), "yyyy-MM-dd"), "2018 - 01 - 25", 3);
+const todo7 = new Todo("Become superhero", format(new Date(), "yyyy-MM-dd"), "2018 - 01 - 25", 3);
 defaultProject.addTodo(todo1);
 defaultProject.addTodo(todo2);
-console.log(defaultProject)
+studyProject.addTodo(todo3);
+workProject.addTodo(todo4);
+defaultProject.addTodo(todo5);
+studyProject.addTodo(todo6);
+workProject.addTodo(todo7);
+
 
 
 // document.querySelector("#add-todo").addEventListener("submit", (e) => {
@@ -48,5 +58,5 @@ console.log(defaultProject)
 
 // document.querySelector(".main__add-todo__selection").innerHTML = app.getProjects.map(i => `<option value="${i.name}">${i.name}</option>`)
 
-UI.updateUI(defaultProject.getTodos());
+UI.updateUI(app.getProjects());
 UI.updateProjects(app.getProjects());
--- src/ui.js
+++ src/ui.js
@@ -1,12 +1,12 @@
 export default class UI {
     static updateUI(todos) {
         let container = '';
-        todos.forEach(item => {
+        todos.forEach(project => project.todos.forEach(item => {
             container +=
                 `<tr>
             <td>${item.title}</td>
             <td>${item.dueDate}</td>
-            <td>Work</td>
+            <td>${project.name}</td>
             <td>${item.date}</td>
             <td>
                 <div class="main__table-priority">
@@ -18,7 +18,7 @@ export default class UI {
             <td class="main__table__edit"><i class="fa-solid fa-pencil"></i></td>
             <td class="main__table__delete"><i class="fa-solid fa-trash-can"></i></td>
         </tr>`
-        })
+        }))
         document.querySelector(".main__todolist").innerHTML = container;
     }
     // static cleanTodoInput() {
